f988b84457488362f22cea48802fa0fbed1b2ccc
Michael Crawley <videoresultsnow@gmail.com>
2017-05-14 09:55:24 -0500

    R Code : Plot #4
    R Code: Load data, subset data, plot, change, create image and save.

---
+++ Plot_4.R
@@ -0,0 +1,45 @@
+## PLOT  OVERVIEW
+
+##Loads “Individual household electric power consumption Data Set”. 
+
+##PLOT 4 Steps
+
+##1 plot4("household_power_consumption.txt") starts the function.
+##2 Load file into R.
+##3 Reformat date column.
+##4 Subset (two days of interest) from (power) data frame, assign to new "df"
+##5 Reformat the Sub_metering,Voltage, Global_active_power, and Global_reactive_power cols (as numeric vectors).
+##6 Add new "timestamp" column to df.
+##7 Use par command to change plot layout from (1,1) to (2,2). 
+##8 Generate each plot. 
+##8 Create 480 x 480 .png image. 
+##9 Save.
+
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
+voltage <- as.numeric(subSetData$Voltage)
+
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
